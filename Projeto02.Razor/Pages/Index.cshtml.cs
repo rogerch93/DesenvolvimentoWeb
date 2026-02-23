8573401b936cc0183335f464baecc59af88501cd
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-02-23 08:26:41 -0300

    Continuação de treinamento basico ASP.NET

--- Projeto02.Razor/Pages/Index.cshtml.cs
+++ Projeto02.Razor/Pages/Index.cshtml.cs
@@ -16,5 +16,25 @@ namespace Projeto02.Razor.Pages
         {
 
         }
+
+        [BindProperty]
+        public string Nome { get; set; }
+        [BindProperty]
+        public int Idade { get; set; }
+        [BindProperty]
+        public bool Lembrar { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if(Idade <= 0)
+            {
+                ModelState.AddModelError(string.Empty, "Preencha a idade corretamente");
+
+                return Page();
+            }
+
+            //retornar a pagiana com dados capturaados
+            return Page();
+        }
     }
 }
