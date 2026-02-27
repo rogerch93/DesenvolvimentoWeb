e90f708acad562917e3ee98a5ca17fb136a5073a
Rogerio Chaves <rogerinhochaves@hotmail.com>
2026-02-27 09:34:20 -0300

    Projeto de conceitos ORM Entity Framework

---
+++ Projeto05.EntityFrameWork/Model/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto05.EntityFrameWork.Model
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+
+    }
+}
---
+++ Projeto05.EntityFrameWork/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Projeto05.EntityFrameWork.Model;
+
+
+//ORM
+//OBJECT RELATIONAL MAPPER
+
+//code-first - o banco de dados não precisa ser criado antes 
+
+//Gerencia a conexão e manipulação das entidades de dados
+public class ApplicationDbContext: DbContext
+{
+    public DbSet<Produto> Produtos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=")
+    }
+}
